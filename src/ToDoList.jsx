60dd38030676010ec556c11a28843973aab7b81c
Thor Sharpless <10237785+dlsharpless@users.noreply.github.com>
2023-10-05 02:37:20 -0600

    Formatted with Prettier

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,19 +1,19 @@
-import { ToDoItem } from "./ToDoItem"
+import { ToDoItem } from "./ToDoItem";
 
 export function ToDoList({ toDos, toggleToDo, deleteToDo }) {
-    return (
-        <ul className="list">
-            {toDos.length === 0 && "No To Do's"}
-            {toDos.map(toDo => {
-                return (
-                    <ToDoItem
-                        {...toDo}
-                        key={toDo.id}
-                        toggleToDo={toggleToDo}
-                        deleteToDo={deleteToDo}
-                    />
-                )
-            })}
-        </ul>
-    )
+  return (
+    <ul className="list">
+      {toDos.length === 0 && "No To Do's"}
+      {toDos.map((toDo) => {
+        return (
+          <ToDoItem
+            {...toDo}
+            key={toDo.id}
+            toggleToDo={toggleToDo}
+            deleteToDo={deleteToDo}
+          />
+        );
+      })}
+    </ul>
+  );
 }
